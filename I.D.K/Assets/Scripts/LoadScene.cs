0e9d34a25f1cc80afc0c36e458bd69fdc3e235ab
Jprummel <jordiprummel@live.nl>
2015-08-31 12:19:59 +0200

    Menu
    -Made placeholder for scenes(Scene files)
    -Made a start for the menu
    -Made buttons in menu work
    
    -JPrummel

---
+++ I.D.K/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//@Author Jordi Prummel
+
+public class LoadScene : MonoBehaviour {
+	public string sceneName;
+
+	public void ChangeLevel()
+	{
+		Application.LoadLevel (sceneName);
+	}
+}
