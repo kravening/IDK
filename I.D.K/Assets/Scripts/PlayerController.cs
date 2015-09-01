fd734e9bec413a78749bd5bc7c39a4048fbe959a
Jprummel <jordiprummel@live.nl>
2015-09-01 10:19:58 +0200

    Movement
    -Added movement
    -Testing platform
    -Added jump
    
    -Jordi

---
+++ I.D.K/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float jumpSpeed;
+	private bool isGrounded;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+
+	void Movement()
+	{
+		//Automatic Movement to the right
+		transform.Translate (Vector2.right * Time.deltaTime * speed);
+		//Jump
+		if (isGrounded) 
+		{
+			if (Input.GetKey (KeyCode.Space)) {
+				transform.Translate (Vector2.up * Time.deltaTime * jumpSpeed);
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.transform.tag == "Ground") 
+		{
+			isGrounded = true;
+		}
+	}
+}
