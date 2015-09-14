bc8796a8f30aa4c4b5a0c47f803284e4db573d40
Jprummel <jordiprummel@live.nl>
2015-09-14 17:57:42 +0200

    Enemyspawn
    -Created enemy set prefabs
    -Enemy spawning script (1 error)
    
    -Jordi

--- I.D.K/Assets/Scripts/Enemies/EnemyManager.cs
+++ I.D.K/Assets/Scripts/Enemies/EnemyManager.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EnemyManager : MonoBehaviour {
+
+    [Header("Enemy set prefabs")]
+    public List<GameObject> enemySet = new List<GameObject>();
     public float enemySpeed;
-    public GameObject enemy;
     public Transform enemySpawner;
     public float spawnCooldown =2;
+    public int cooldownSpawner = 0;
+    private int counter = 0;
     //public float numberOfEnemiesSpawn;
 	// Use this for initialization
 	void Start () {
@@ -15,7 +20,7 @@ public class EnemyManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         EnemyMove();
-        SpawnEnemy();
+        //SpawnEnemy();
 	}
 
     void EnemyMove()
@@ -35,11 +40,38 @@ public class EnemyManager : MonoBehaviour {
         }
     }
 
-    void SpawnEnemy()
+/*    void SpawnEnemy()
     {
-       Debug.Log("Spawned enemy");
-        Instantiate(enemy,enemySpawner.position,Quaternion.Euler(0,0,0));
-        SpawnCooldown();
+        if(counter >= cooldownSpawner){
+          //SpawnCooldown();
+             Debug.Log("Spawned enemy");
+            Instantiate(,enemySpawner.position,Quaternion.Euler(0,0,0));
+            counter= 0;
+
+        }else{
+            counter++;
+        }
+        
+    }
+*/
+    private GameObject getRandomEnemySet(Vector3 _position)
+    {
+        return spawnEnemySet(enemySet[Random.Range(0, enemySet.Count)], _position);
+    }
+
+    private GameObject spawnEnemySet(GameObject _enemy, Vector3 _position)
+    {
+         if(counter >= cooldownSpawner){
+          //SpawnCooldown();
+             Debug.Log("Spawned enemy");
+             Instantiate(_enemy,enemySpawner.position,Quaternion.Euler(0,0,0));
+             counter = 0;
+
+         }else
+         {
+            counter++;
+         }
+        
     }
 
     IEnumerator SpawnCooldown()
