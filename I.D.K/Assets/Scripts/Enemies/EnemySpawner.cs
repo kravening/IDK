004fed66f37f4a715b03b1334b39edd669abf4d7
Jprummel <jordiprummel@live.nl>
2015-09-15 17:07:47 +0200

    Enemy Spawner done
    -Finished enemy spawning (can still be changed later on)
    
    -Jordi Prummel

--- I.D.K/Assets/Scripts/Enemies/EnemySpawner.cs
+++ I.D.K/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,30 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
-public class EnemySpawner : MonoBehaviour 
-{
-    public List<GameObject> enemyChunks = new List<GameObject>();
-    public GameObject enemy;
-	// Use this for initialization
-	void Start () 
-    {
-        Spawn();
+public class EnemySpawner : MonoBehaviour {
+
+    private float xMin = 10;
+    private float xMax = 20;
+    private float yMin = 0;
+    private float yMax = 3;
+
+    private float spawnCooldown = 4;
+    private float timeUntilSpawn = 4;
+
+    public GameObject[] enemyPrefabs;
+   // Use this for initialization
+	void Start () {
+        for (int i = 0; i < 1; i++)
+        {
+            SpawnEnemy();
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () 
-    {
-	
+	void Update () {
+	         timeUntilSpawn -= Time.deltaTime;
+     if(timeUntilSpawn <= 0)
+     {
+         // Do your enemy spawns here
+         SpawnEnemy();
+         // Reset for next spawn
+         timeUntilSpawn = spawnCooldown;
+     }
 	}
 
-    void Spawn()
+    private void SpawnEnemy()
     {
-        for (int i = 0; i < enemyChunks.Count; i++)
-        {
-            enemyChunks.Add(enemy);
-            Debug.Log(enemyChunks.Count);
-        }
+        GameObject enemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+	
+        Vector3 newPos = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), 0);
+        GameObject octo = Instantiate(enemyPrefab, newPos, Quaternion.identity) as GameObject;
     }
-
 }
